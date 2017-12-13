834e02fa3ddba86041d521a46edce46c84fe6b46
Isaac960 <danielramireznapoles@yahoo.com>
2017-12-12 21:00:35 -0600

    funcion suma

--- hola.cpp
+++ hola.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 void main(){
 	
+	int a,b,r;
 	cout<<"hola mundo";
-	
+	cout<<"ingresa tu primer numero";
+	cin>>a;
+	cout<<"ingresa tu segundo numero";
+	cin>>b;
+	r=a+b;
+	cout<<"suma"<<r;
 }
